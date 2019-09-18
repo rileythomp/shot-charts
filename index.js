df16530c6057ea235dcb498446d2ca5614081fd2
rileythomp <jr3thomp@edu.uwaterloo.ca>
2019-09-18 12:48:54 -0400

    web interactive

--- index.js
+++ index.js
@@ -1,150 +1,23 @@
 'use strict'
 
-const shell = require('shelljs');
-const nba = require('nba');
-const replaceColor = require('replace-color');
-
+const shotChart = require('./shotchart')
 var express = require('express');
+
 var app = express();
-
-const PORT = process.env.PORT || 3000;
-
+app.use(express.urlencoded());
+app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
     console.log('Running on localhost:' + PORT);
 });
 
-const length = 500;
+app.post('/newplayerchart', function(req, res) {
+	shotChart.create_chart(req.body.name);
 
-const partitions = 30;
-
-const partition_length = length/partitions;
-
-const num_cells = partitions*partitions;
-
-const topY = 450;
-const leftX = -250;
-const bottomY = -50;
-const rightX = 250;
-
-const player = nba.findPlayer('Russell Westbrook');
-
-let params = {
-	PlayerID: player.playerId
-}
-
-let heatMap = {};
-
-for (let i = 1; i <= num_cells; ++i) {
-	heatMap[i] = {made: 0, missed: 0};
-}
-
-function replace_color(image, replace) {
-	replaceColor({
-		image: './public/court_bits/' + image + '.png',
-		colors: {
-			type: 'rgb',
-			targetColor: [255, 255, 255],
-			replaceColor: replace
-		},
-		deltaE: 10
-	})
-	.then((jimpObject) => {
-		jimpObject.write('./public/court_bits/' + image + '.png', (err) => {
-			if (err) { return console.log(err) }
-		})
-	})
-	.catch((err) => {
-		console.log(err);
-	})
-}
-
-function colorFromPercent(percent) {
-	if (percent > 0 && percent <= 0.1) {
-		return [255, 0, 0];
-	}
-	else if (percent >= 0.1 && percent <= 0.2) {
-		return [255, 87, 0];
-	}
-	else if (percent >= 0.2 && percent <= 0.3) {
-		return [255, 175, 0];
-	}
-	else if (percent >= 0.3 && percent <= 0.4) {
-		return [255, 255, 0];
-	}
-	else if (percent >= 0.4 && percent <= 0.5) {
-		return [159, 255, 86];
-	}
-	else if (percent >= 0.5 && percent <= 0.6) {
-		return [71, 255, 0];
-	}
-	else if (percent >= 0.6) {
-		return [0, 255, 0];
-	}
-	else {
-		return [222, 184, 135];
-	}
-}
-
-function updateMap(region, shot) {
-	if (shot.shotMadeFlag == 1) {
-		heatMap[region].made += 1;
-	}
-	else {
-		heatMap[region].missed += 1;
-	}
-}
-
-function abs(x) {
-	return (x >= 0 ? x : -1*x);
-}
-
-
-
-nba.stats.shots(params).then((res) => {
-	let shots = res.shot_Chart_Detail;
-	let num_shots = shots.length;
-
-	// shell.exec('./move_pics_script', function(code, stdout, stderr) {
-	// 	console.log('Exit code:', code);
-	// 	console.log('Program output:', stdout);
-	// 	console.log('Program stderr:', stderr);
-	// });
-
-	for (let i = 0; i < num_shots; ++i) {
-		let shot = shots[i];
-		let yLoc = shot.locY;
-		let xLoc = shot.locX;
-
-		if (yLoc > topY) {
-			yLoc = topY;
-		}
-		else if (yLoc < bottomY) {
-			yLoc = bottomY;
-		}
-
-		if (xLoc > rightX) {
-			xLoc = rightX;
-		}
-		else if (xLoc < leftX) {
-			xLoc = leftX;
-		}
-
-		// 0 is far right, 9 is far left
-		let col = Math.floor(abs((leftX - xLoc)/partition_length));
-
-		// 0 is top, 9 is bottom
-		let row = Math.floor(abs((topY - yLoc)/partition_length));
-
-		let index = row * partitions + col + 1;
-		updateMap(index, shot);
-	}
-
-	for (let i = 1; i <= num_cells; ++i) {
-		let percent = heatMap[i].made / (heatMap[i].made + heatMap[i].missed);
-
-		replace_color(i, colorFromPercent(percent));
-	}
+	// hacky way to wait for all cells color to be updated
+	setTimeout(function() {
+		res.send(200)
+	}, 7500);
 });
-
--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 let map = document.getElementById('map');
 const partitions = 30;
 
+$('#loader').hide();
 
 for (let i = 7; i < partitions-2; ++i) {
     let row = document.createElement('tr');
@@ -14,3 +15,28 @@ for (let i = 7; i < partitions-2; ++i) {
     }
     map.appendChild(row);
 }
+
+$('#nameform').submit(function(e){  
+    e.preventDefault();
+    $('#loader').show();
+    let pname = $('#playername').val();
+    
+    $.ajax({ 
+        url: '/newplayerchart',
+        type: 'POST',
+        cache: false, 
+        data: {name: pname}, 
+        success: function(data){
+            localStorage.setItem('player_name', pname);
+            $('#loader').hide();
+            location = location;
+        }
+        , error: function(jqXHR, textStatus, err){
+            console.error("Error: ", textStatus, err);
+        }
+    })
+});  
+
+$(window).on('load', function() {
+    $('#chart-description').html(localStorage.getItem('player_name'));
+})
