4389f7267e347ae2b2380bb678add404b5e014fe
rileythomp <jr3thomp@edu.uwaterloo.ca>
2019-09-23 14:42:28 -0400

    add logging

--- index.js
+++ index.js
@@ -11,21 +11,27 @@ app.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
-    console.log('Running on localhost:' + PORT);
+    console.log('Running on port ' + PORT);
 });
 
 app.post('/newplayerchart', function(req, res) {
 	let player = nba.findPlayer(req.body.name);
 
 	if (player === undefined) {
+		console.log('sending 500');
 		res.status(500).send('Player not defined');
+		console.log('sending 500');
 	}
 	else {
+		console.log('creating chart');
 		shotChart.create_chart(player);
+		console.log('done creating chart');
 
 		// hacky way to wait for all cells color to be updated
 		setTimeout(function() {
+			console.log('sending 200');
 			res.sendStatus(200)
+			console.log('sent 200');
 		}, 7500);
 	}
 
--- shotchart.js
+++ shotchart.js
@@ -148,5 +148,6 @@ exports.create_chart = function(player) {
 			let percent = heatMap[i].made / (heatMap[i].made + heatMap[i].missed);
 			replace_color(i, color_from_percent(percent));
 		}
+		console.log('done replacing colors');
 	});
 }
