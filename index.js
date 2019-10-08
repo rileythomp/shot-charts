d0fa0559ba1878a1f8f1a04feef5bcd89dec85f2
rileythomp <jr3thomp@edu.uwaterloo.ca>
2019-10-08 16:48:48 -0400

    fix heroku

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-const PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Running on port ' + PORT);
 });
